d581f822cb9cd4d04c62c5fce7f885a2ae172b66
JBeckius <jbeckius91@gmail.com>
2016-08-02 14:57:36 -0400

    Changed server to use both static and dynamic ports.

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ var ex = require("./lyrics.js");
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use(express.static(__dirname + "/public"));
 //if we get a request at /api/lyric, do the thing.
 app.get('/api/lyric', function(request, response) {
   response.send(ex[0]);
 });
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(app.get('port'), function () {
   var port = server.address().port;
 
-  console.log('Example app listening at port', port);
+  console.log('Example app listening at port', app.get('port'));
 })
 
 //A change
