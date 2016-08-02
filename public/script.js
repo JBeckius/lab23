6f9650a9006faebacebc4d679c45496fa0920dac
JBeckius <jbeckius91@gmail.com>
2016-08-02 14:36:44 -0400

    added more comments.

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 var lyric = '';
-
+//get something from /api/lyric
+//Confusing!! .get does different things in JQ and express!
 $.get('/api/lyric', function(data) {
   lyric = data;
   console.log(lyric);
